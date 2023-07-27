bc0c0fb4926cab7e3425cafe5e04dca87f370ed3
Norbert Busom <norbertbg@outlook.com>
2023-07-27 12:23:50 +0200

    cors changed, vanessa

--- config/index.js
+++ config/index.js
@@ -15,21 +15,23 @@ const cookieParser = require("cookie-parser");
 // unless the request is made from the same domain, by default express wont accept POST requests
 const cors = require("cors");
 
-const FRONTEND_URL = process.env.ORIGIN || "http://localhost:3000";
-
-// Middleware configuration
 module.exports = (app) => {
   // Because this will be hosted on a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
   // Services like Fly use something called a proxy and you need to add this to your server
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
-  app.use(
-    cors({
-      origin: [FRONTEND_URL],
-    })
-  );
-
+  const allowedOrigins = [ "http://localhost:3000" ];
+  app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  }));
+  
   // In development environment the app logs
   app.use(logger("dev"));
 
